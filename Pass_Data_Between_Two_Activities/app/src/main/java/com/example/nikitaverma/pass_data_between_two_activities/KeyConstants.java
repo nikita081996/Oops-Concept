7c4d1adc0be688b10d2ee321f0201c4037fc4df1
nikks081996 <39641014+nikks081996@users.noreply.github.com>
2018-06-17 12:29:46 +0530

    Pass data between two activities
    Pass data between two activities

--- Pass_Data_Between_Two_Activities/app/src/main/java/com/example/nikitaverma/pass_data_between_two_activities/KeyConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.nikitaverma.pass_data_between_two_activities;
-
-public interface KeyConstants {
-    String KEY_NAME = "name";
-    String KEY_ADDRESS = "address";
-    String KEY_EMAIL = "email";
-
-}
